4cf7c896935b7b9cb4c62db4a022be64dc8144e6
siochy <37552352+siochy@users.noreply.github.com>
2024-03-01 23:39:33 +0500

    Update fib_gen.py
    Поменял выдачу fib_num2 на fib_num1, чтобы учитывать первый элемент. Добавил docstring

--- fibo/fib_gen.py
+++ fibo/fib_gen.py
@@ -1,10 +1,14 @@
-from decor.decor import decor
+from lesson_2.decor.decor import decor
 
 
 @decor
 def fib_gen(count):
+    """Generate Fibonacci sequence.
+    Fibonacci numbers with loop and
+    decorator that shows time of process,
+    argument and type"""
     fib_num1 = 0
     fib_num2 = 1
     for i in range(count):
         fib_num1, fib_num2 = fib_num2, fib_num1 + fib_num2
-        yield fib_num2
+        yield fib_num1
