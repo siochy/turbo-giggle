c9e5ef5e284788aedd2fb3841fc655ac819b0b3e
siochy <37552352+siochy@users.noreply.github.com>
2024-03-01 22:07:39 +0500

    Update lesson_first_p1.py
    Добавил docstrings и поменял чтение файла на построчное

--- lesson_first_p1.py
+++ lesson_first_p1.py
@@ -3,6 +3,12 @@ import string
 
 
 def generate_random_text():
+    """Generates text for binary file.
+    Takes symbols 'a..Z', '0..9', '!..+' and spaces.
+    Then encodes this with utf-8 and write into the .bin file
+    'some_text.bin'.
+    Uses for tests. You can take your own text in some_text.bin file.
+    """
     rand_symbols = string.printable
     rand_text = ''.join(random.choice(rand_symbols) for i in range(random.randint(1000, 5000)))
     rand_text_bin = rand_text.encode('utf-8')
@@ -11,22 +17,24 @@ def generate_random_text():
 
 
 def reading_bin_file():
+    """Counts symbols, words and lines in binary file.
+    Opens some_text.bin file line by line then
+    decodes it and counts symbols, words, lines from file."""
     with open('some_text.bin', 'rb') as file:
-        text_from_file_bin = file.readlines()
         symbols = 0
         words = 0
-        lines = len(text_from_file_bin)
-        for line in text_from_file_bin:
-            symbols += len(line.decode('utf-8'))
-            words += len(line.decode('utf-8').split())
-        file.seek(0)
-        text_from_file_str = file.read().decode('utf-8')
+        lines = 0
+        for line in file:
+            text_line = line.decode('utf-8')
+            symbols += len(text_line)
+            words += len(text_line.split())
+            lines += 1
 
-    print(f'Text from file:\n{text_from_file_str}')
     print(f'Symbols: {symbols}')
     print(f'Words: {words}')
     print(f'Lines: {lines}')
 
 
-generate_random_text()
-reading_bin_file()
+if __name__ == '__main__':
+    generate_random_text()
+    reading_bin_file()
