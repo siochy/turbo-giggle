e416e83529bc183887a3e40777b416072e13eff1
siochy <37552352+siochy@users.noreply.github.com>
2024-03-01 23:36:03 +0500

    Update main.py
    Убрал list, переместил docstring в функцию

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-'''
-Shows list of fibonacci with random number with showing info
-about time, number in generator, name and type of it.
-'''
-
-
 from random import randint
 from fibo.fib_gen import fib_gen
 from fibo.fib_iter import FibIter
@@ -12,6 +6,8 @@ from cronos import cronos
 
 
 def show_fib():
+    """Shows list of fibonacci with random number with showing info
+    about time, number in generator, name and type of it."""
     num_random = randint(1, 50)
     fibo = list(fib_gen(num_random))
     for number in fibo:
