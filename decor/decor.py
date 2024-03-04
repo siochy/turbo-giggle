c44bb96979568b13bbde3cf6c59ab83d94005aa1
siochy <37552352+siochy@users.noreply.github.com>
2024-03-04 15:33:16 +0500

    Update decor.py
    Вернул вызов функции для подсчёта времени, поставил result в return

--- decor/decor.py
+++ decor/decor.py
@@ -5,12 +5,12 @@ def decor(func):
     """Decorator that shows name, argument, type and result of function."""
     def insider(arg):
         start_func = time.time()
+        result = func(arg)
         end_func = time.time()
         func_time = start_func - end_func
-        message = f'''Function {str(func.__name__)} is running by argument {arg}
+        print(f'''Function {str(func.__name__)} is running by argument {arg}
                     with time {func_time}s
                     and resulting type {str(type(func(arg)))}
-                    Result:
-                    {list(func(arg))}'''
-        return message
+                    Result:''')
+        return result
     return insider
